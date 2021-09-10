91f93d7d0c0387879b795c9e5511c9fbd737023a
Anthony Yarbrough <asyarb@hawaii.edu>
2021-09-10 11:15:46 -1000

    feat: setup schemas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,26 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		fontSize: {
+			8: "8",
+			10: "10",
+			12: "12",
+			13: "13",
 			16: "16",
+			18: "18",
+			22: "22",
+			28: "28",
+			32: "32",
+			42: "42",
+			52: "52",
+			62: "62",
 		},
 		lineHeight: {
 			1: "1",
+			"1_15": "1.15",
 		},
 		fontFamily: {
 			sans: ["Work SansVariable", "system-ui"],
+			sansExt: ["monotype-grotesque-extended", "system-ui"],
 			serif: ["bely-display", "serif"],
 		},
 
@@ -27,6 +40,13 @@ module.exports = {
 					capHeight: 660,
 					ascent: 930,
 					descent: -243,
+					lineGap: 0,
+					unitsPerEm: 1000,
+				},
+				sansExt: {
+					capHeight: 694,
+					ascent: 981,
+					descent: -252,
 					lineGap: 0,
 					unitsPerEm: 1000,
 				},
