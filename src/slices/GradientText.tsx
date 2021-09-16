e39ad15ebf2e66fd3a61d7c884d05d8a05fb738b
Anthony Yarbrough <asyarb@hawaii.edu>
2021-09-16 11:22:33 -1000

    fix: large desktop sizes

--- src/slices/GradientText.tsx
+++ src/slices/GradientText.tsx
@@ -12,10 +12,22 @@ export const sliceType = "PrismicPageDataBodyGradientText"
 
 const GradientText = ({ text }: ReturnType<typeof mapDataToProps>) => {
 	return (
-		<section className="relative aspect-w-10 aspect-h-16">
-			<Gradient className="absolute inset-0" />
+		<section
+			className={clsx(
+				"relative aspect-w-10 aspect-h-16",
+				"md:aspect-w-16 md:aspect-h-9",
+				"2xl:aspect-none 2xl:h-[800px]",
+				"2xl:relative"
+			)}
+		>
+			<Gradient className="absolute inset-0 2xl:absolute 2xl:inset-0 2xl:w-full 2xl:h-full" />
 
-			<div className="flex items-center justify-center w-full h-full px-8">
+			<div
+				className={clsx(
+					"flex items-center justify-center w-full h-full px-8",
+					"2xl:w-full 2xl:h-full"
+				)}
+			>
 				<h2 className={clsx(serif, "relative text-beige-92 text-center")}>
 					{text}
 				</h2>
