3499b184131b624efaffbe13917bdda45ded53ca
Anthony Yarbrough <asyarb@hawaii.edu>
2021-09-21 10:41:47 -1000

    feat: mobile sponsor

--- src/slices/Sponsors.tsx
+++ src/slices/Sponsors.tsx
@@ -7,18 +7,173 @@ import type { SponsorsFragment } from "../gqlTypes.gen"
 
 import { BoundedBox } from "../components/BoundedBox"
 import { usePrismicSponsors } from "../hooks/usePrismicSponsors"
+import { Image } from "../components/Image"
 
 export const sliceType = "PrismicPageDataBodySponsors"
+
+const SponsorHeading = ({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<"h5">) => {
+	return (
+		<h5
+			className={clsx(
+				className,
+				"font-sans",
+				"text-center",
+				"text-18",
+				"leading-1_3 font-semibold",
+				"max-w-[20ch]"
+			)}
+			{...props}
+		/>
+	)
+}
+
+interface SponsorContainerProps extends React.ComponentPropsWithoutRef<"div"> {
+	heading: string
+}
+
+const SponsorContainer = ({
+	className,
+	children,
+	heading,
+	...props
+}: SponsorContainerProps) => {
+	return (
+		<div
+			className={clsx(className, "flex flex-col space-y-6 items-center")}
+			{...props}
+		>
+			<SponsorHeading>{heading}</SponsorHeading>
+
+			{children}
+		</div>
+	)
+}
+
+const RainbowsSponsor = (props: React.ComponentPropsWithoutRef<"div">) => {
+	const sponsors = usePrismicSponsors()
+	const [rainbowSponsor] = sponsors.rainbowsOverWaikiki
+
+	return (
+		<SponsorContainer
+			heading="Rainbows Over Waikiki Presenting Sponsor"
+			{...props}
+		>
+			{rainbowSponsor && (
+				<Image
+					src={rainbowSponsor.imageUrl}
+					alt={rainbowSponsor.imageAlt ?? rainbowSponsor.name}
+					className="max-w-[250px] w-full"
+				/>
+			)}
+		</SponsorContainer>
+	)
+}
+
+const VisionarySponsors = (props: React.ComponentPropsWithoutRef<"div">) => {
+	const { visionary } = usePrismicSponsors()
+
+	return (
+		<SponsorContainer heading="Visionary" {...props}>
+			<ul className="space-y-10">
+				{visionary.map((sponsor, idx) => (
+					<li key={`visionarySponsor-${idx}`} className="max-w-[225px] w-full">
+						<Image
+							src={sponsor.imageUrl}
+							alt={sponsor.imageAlt ?? sponsor.name}
+						/>
+					</li>
+				))}
+			</ul>
+		</SponsorContainer>
+	)
+}
+
+const GoldSponsors = (props: React.ComponentPropsWithoutRef<"div">) => {
+	const { gold } = usePrismicSponsors()
+
+	return (
+		<SponsorContainer heading="Gold" {...props}>
+			<ul className="flex flex-wrap items-center justify-center gap-10">
+				{gold.map((sponsor, idx) => (
+					<li key={`goldSponsor-${idx}`} className="w-[calc(50%-40px)]">
+						<Image
+							src={sponsor.imageUrl}
+							alt={sponsor.imageAlt ?? sponsor.name}
+						/>
+					</li>
+				))}
+			</ul>
+		</SponsorContainer>
+	)
+}
+
+const SilverSponsors = (props: React.ComponentPropsWithoutRef<"div">) => {
+	const { silver } = usePrismicSponsors()
+
+	return (
+		<SponsorContainer heading="Silver" {...props}>
+			<ul className="flex flex-wrap items-center justify-center gap-10">
+				{silver.map((sponsor, idx) => (
+					<li key={`silverSponsor-${idx}`} className="w-[calc(33%-40px)]">
+						<Image
+							src={sponsor.imageUrl}
+							alt={sponsor.imageAlt ?? sponsor.name}
+						/>
+					</li>
+				))}
+			</ul>
+		</SponsorContainer>
+	)
+}
+
+const BronzeSponsors = (props: React.ComponentPropsWithoutRef<"div">) => {
+	const { bronze } = usePrismicSponsors()
+
+	return (
+		<SponsorContainer heading="Bronze" {...props}>
+			<ul className="flex flex-wrap items-center justify-center gap-10">
+				{bronze.map((sponsor, idx) => (
+					<li key={`bronzeSponsor-${idx}`} className="w-[calc(33%-40px)]">
+						<Image
+							src={sponsor.imageUrl}
+							alt={sponsor.imageAlt ?? sponsor.name}
+						/>
+					</li>
+				))}
+			</ul>
+		</SponsorContainer>
+	)
+}
+
+const PartnerSponsors = (props: React.ComponentPropsWithoutRef<"div">) => {
+	const { partner } = usePrismicSponsors()
+
+	return (
+		<SponsorContainer heading="Partners" {...props}>
+			<ul className="flex flex-wrap items-center justify-center gap-10">
+				{partner.map((sponsor, idx) => (
+					<li key={`partnerSponsor-${idx}`} className="w-[calc(33%-40px)]">
+						<Image
+							src={sponsor.imageUrl}
+							alt={sponsor.imageAlt ?? sponsor.name}
+						/>
+					</li>
+				))}
+			</ul>
+		</SponsorContainer>
+	)
+}
 
 const Sponsors = ({
 	subheading,
 	heading,
 }: ReturnType<typeof mapDataToProps>) => {
-	const sponsors = usePrismicSponsors()
-
 	return (
 		<BoundedBox tag="section">
-			<div className="flex flex-col items-center space-y-4 text-center">
+			<div className="flex flex-col items-center space-y-4 text-center mb-14">
 				{subheading && (
 					<h4
 						className={clsx(
@@ -29,6 +184,7 @@ const Sponsors = ({
 						{subheading}
 					</h4>
 				)}
+
 				{heading && (
 					<h2
 						className={clsx(
@@ -40,6 +196,15 @@ const Sponsors = ({
 						{heading}
 					</h2>
 				)}
+			</div>
+
+			<div className="space-y-16">
+				<RainbowsSponsor />
+				<VisionarySponsors />
+				<GoldSponsors />
+				<SilverSponsors />
+				<BronzeSponsors />
+				<PartnerSponsors />
 			</div>
 		</BoundedBox>
 	)
