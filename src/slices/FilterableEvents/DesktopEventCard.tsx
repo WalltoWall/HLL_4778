18b84194d3afd25eee25459cd004892133904eee
Anthony Yarbrough <asyarb@hawaii.edu>
2021-09-22 11:06:38 -1000

    refactor: pass event obj instead of individual properties

--- src/slices/FilterableEvents/DesktopEventCard.tsx
+++ src/slices/FilterableEvents/DesktopEventCard.tsx
@@ -8,18 +8,13 @@ import type { EventCardProps } from "./MobileEventCard"
 type DesktopEventCardProps = EventCardProps
 
 export const DesktopEventCard = ({
-	color,
-	title,
-	descriptionHTML,
-	date,
-	illustration,
-	href,
+	event,
 	updateActiveEvent,
 	activeVariant,
 }: DesktopEventCardProps) => {
 	const { ref } = useUpdateEventInView({
 		updateEvent: updateActiveEvent,
-		event: { color, date, illustration, descriptionHTML, href, title },
+		event,
 		threshold: 0.33,
 	})
 
@@ -27,10 +22,10 @@ export const DesktopEventCard = ({
 		<div className="h-screen col-start-1" ref={ref}>
 			<EventDescription
 				activeVariant={activeVariant}
-				date={date}
-				descriptionHTML={descriptionHTML}
-				title={title}
-				href={href}
+				date={event.date}
+				descriptionHTML={event.descriptionHTML}
+				title={event.title}
+				href={event.href}
 			/>
 		</div>
 	)
