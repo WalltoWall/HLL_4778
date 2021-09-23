daeaa56ff07788c69f12a15b17ac8a557e24d7ac
Anthony Yarbrough <asyarb@hawaii.edu>
2021-09-22 15:34:55 -1000

    feat: mobile menu button animation

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,20 +1,64 @@
 import * as React from "react"
 import clsx from "clsx"
+import { m, useAnimation } from "framer-motion"
 
-const Line = () => <div className="bg-beige-92 w-[30px] h-[3px] rounded-full" />
+const Line = ({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<typeof m.div>) => (
+	<m.div
+		className={clsx("bg-beige-92 w-[30px] h-[3px] rounded-full", className)}
+		{...props}
+	/>
+)
+
+interface Props extends React.ComponentPropsWithoutRef<"button"> {
+	isOpen: boolean
+}
 
 export const MenuButton = ({
 	className,
 	children,
+	isOpen,
 	...props
-}: React.ComponentPropsWithoutRef<"button">) => {
+}: Props) => {
+	const topLineControls = useAnimation()
+	const bottomLineControls = useAnimation()
+
+	React.useEffect(() => {
+		async function asyncEffect() {
+			if (isOpen) {
+				await Promise.all([
+					topLineControls.start({ y: 6 }),
+					bottomLineControls.start({ y: -6 }),
+				])
+				await Promise.all([
+					topLineControls.start({ rotate: -45 }),
+					bottomLineControls.start({ rotate: 45 }),
+				])
+			} else {
+				await Promise.all([
+					topLineControls.start({ rotate: 0 }),
+					bottomLineControls.start({ rotate: 0 }),
+				])
+				await Promise.all([
+					topLineControls.start({ y: 0 }),
+					bottomLineControls.start({ y: 0 }),
+				])
+			}
+		}
+
+		asyncEffect()
+	}, [isOpen])
+
 	return (
 		<button
 			className={clsx("grid gap-y-[10px]", "focus:ring", className)}
 			{...props}
 		>
-			<Line />
-			<Line />
+			<Line animate={topLineControls} />
+			<Line animate={bottomLineControls} />
+
 			{children}
 		</button>
 	)
