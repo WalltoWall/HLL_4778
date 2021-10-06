bb57c1e356eb525c9d10293406b842166c844c1c
Anthony Yarbrough <asyarb@hawaii.edu>
2021-10-05 16:58:27 -1000

    feat: seo and perf improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
 		"gatsby-plugin-postcss",
 		"gatsby-plugin-catch-links",
 		"gatsby-plugin-sitemap",
+		"gatsby-plugin-loadable-components-ssr",
 		{
 			resolve: "gatsby-plugin-manifest",
 			options: {
