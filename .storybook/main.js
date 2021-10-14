4b301885d61e5ce396bf15853bf1e0414becdb58
Anthony Yarbrough <asyarb@hawaii.edu>
2021-10-14 09:33:13 -1000

    feat: install

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+	core: {
+		builder: "webpack5",
+	},
+	features: {
+		previewCsfV3: true,
+	},
+	stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"storybook-addon-gatsby",
+	],
+}
